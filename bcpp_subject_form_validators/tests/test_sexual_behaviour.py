3545559f444a455aa8491d2c02b4bb5f44dcf7f3
aseokana <aseokana@bhp.org.bw>
2017-07-31 19:16:18 +0200

    tests for sexual_behaviour validators

--- bcpp_subject_form_validators/tests/test_sexual_behaviour.py
+++ bcpp_subject_form_validators/tests/test_sexual_behaviour.py
@@ -8,6 +8,7 @@ from edc_constants.constants import MALE, NO
 from .models import SubjectVisit
 
 from ..form_validators import SexualBehaviourFormValidator
+from edc_base.utils import get_utcnow
 
 
 class TestSexualBehaviourFormValidator(TestCase):
@@ -40,6 +41,20 @@ class TestSexualBehaviourFormValidator(TestCase):
             subject_visit=self.subject_visit,
             ever_sex=NO,
             last_year_partners=1)
+        form_validator = SexualBehaviourFormValidator(
+            cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn(NOT_REQUIRED_ERROR, form_validator._error_codes)
+
+    @tag('111')
+    def test_first_sex_no(self):
+        cleaned_data = dict(
+            subject_visit=self.subject_visit,
+            ever_sex=NO,
+            first_sex=get_utcnow)
         form_validator = SexualBehaviourFormValidator(
             cleaned_data=cleaned_data)
         try:
