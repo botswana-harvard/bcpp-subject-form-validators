03525715a25501e1c8080a50b90f29d9ba119be4
aseokana <aseokana@bhp.org.bw>
2017-07-31 11:42:55 +0200

    pima_vl form validator tests

--- bcpp_subject_form_validators/tests/test_pima_vl.py
+++ bcpp_subject_form_validators/tests/test_pima_vl.py
@@ -19,7 +19,7 @@ class TestPimaCd4FormValidator(TestCase):
         self.subject_visit = SubjectVisit.objects.create(
             subject_identifier=self.subject_identifier)
 
-    def test_location1(self):
+    def test_location(self):
         cleaned_data = dict(
             subject_visit=self.subject_visit,
             test_done=YES,
@@ -28,7 +28,16 @@ class TestPimaCd4FormValidator(TestCase):
             cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
 
-    def test_location2(self):
+    def test_datetime(self):
+        cleaned_data = dict(
+            subject_visit=self.subject_visit,
+            test_done=YES,
+            test_datetime=None)
+        form_validator = PimaVlFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_required_fields(self):
         cleaned_data = dict(
             test_done=YES,
             location='Mmathethe',
