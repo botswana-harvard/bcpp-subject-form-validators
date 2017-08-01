49a4c7b6a6cf522a6b7e7cbb7be3f96ab6019659
erikvw <ew2789@gmail.com>
2017-08-01 09:55:06 +0200

    education

---
+++ bcpp_subject_form_validators/tests/test_education.py
@@ -0,0 +1,76 @@
+from arrow.arrow import Arrow
+from dateutil.relativedelta import relativedelta
+from django import forms
+from django.test import TestCase
+
+from edc_base.modelform_validators import REQUIRED_ERROR, NOT_REQUIRED_ERROR
+from edc_constants.constants import MALE, YES, NO, NOT_APPLICABLE, OTHER, DWTA
+from edc_registration.models import RegisteredSubject
+
+from ..form_validators import EducationFormValidator
+from .models import SubjectVisit, ListModel
+from bcpp_subject_form_validators.tests.models import SubjectLocator
+
+
+class TestValidators(TestCase):
+
+    def setUp(self):
+        self.subject_identifier = '12345'
+        RegisteredSubject.objects.create(
+            subject_identifier=self.subject_identifier,
+            gender=MALE)
+        self.subject_visit = SubjectVisit.objects.create(
+            subject_identifier=self.subject_identifier)
+        EducationFormValidator.subject_locator_model = 'bcpp_subject_form_validators.subjectlocator'
+        self.subject_locator = SubjectLocator.objects.create(
+            subject_identifier=self.subject_identifier,
+            may_call_work=YES)
+
+    def test_ok(self):
+        cleaned_data = dict(
+            subject_visit=self.subject_visit)
+        form_validator = EducationFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertEqual({}, form_validator._errors)
+
+    def test_ok_no_locator(self):
+        SubjectLocator.objects.all().delete()
+        cleaned_data = dict(
+            subject_visit=self.subject_visit)
+        form_validator = EducationFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertEqual({}, form_validator._errors)
+
+    def test_working_on_locator(self):
+        cleaned_data = dict(
+            subject_visit=self.subject_visit,
+            working=NO)
+        form_validator = EducationFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('working', form_validator._errors)
+
+    def test_unemployed(self):
+        self.subject_locator.may_call_work = NO
+        self.subject_locator.save()
+        for response in ['student', 'retired']:
+            with self.subTest(response=response):
+                cleaned_data = dict(
+                    subject_visit=self.subject_visit,
+                    working=NO,
+                    reason_unemployed=response)
+                form_validator = EducationFormValidator(
+                    cleaned_data=cleaned_data)
+                try:
+                    form_validator.validate()
+                except forms.ValidationError:
+                    pass
+                self.assertIn('monthly_income', form_validator._errors)
