f6b209f61976fd96e1f255b7f60a6a01ae2b2361
Gofaone Magodi <magodign@magodign.local>
2017-08-15 09:30:32 +0200

    allow income for employed

--- bcpp_subject_form_validators/tests/test_education.py
+++ bcpp_subject_form_validators/tests/test_education.py
@@ -86,20 +86,3 @@ class TestValidators(TestCase):
         except forms.ValidationError:
             pass
         self.assertEqual({}, form_validator._errors)
-
-    def test_unemployed(self):
-        self.subject_locator.may_call_work = NO
-        self.subject_locator.save()
-        for response in ['student', 'retired']:
-            with self.subTest(response=response):
-                cleaned_data = dict(
-                    subject_visit=self.subject_visit,
-                    working=NO,
-                    reason_unemployed=response)
-                form_validator = EducationFormValidator(
-                    cleaned_data=cleaned_data)
-                try:
-                    form_validator.validate()
-                except forms.ValidationError:
-                    pass
-                self.assertIn('monthly_income', form_validator._errors)
