f72fa1ac085850be3b4e3b2dec3e4189fb6d2d52
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-31 16:53:21 +0200

    update validations

--- bcpp_subject_form_validators/form_validators/reproductive_health.py
+++ bcpp_subject_form_validators/form_validators/reproductive_health.py
@@ -1,5 +1,5 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import YES
+from edc_constants.constants import YES, NO
 
 
 class ReproductiveHealthFormValidator(FormValidator):
@@ -7,13 +7,21 @@ class ReproductiveHealthFormValidator(FormValidator):
     def clean(self):
         self.not_required_if(
             YES, field='menopause', field_required='family_planning')
-        self.required_if(
+        self.not_required_if(
             YES, field='menopause', field_required='currently_pregnant')
+        self.required_if(
+            NO,
+            field='menopause',
+            field_required='currently_pregnant'
+        )
 
         self.validate_other_specify(field='family_planning')
 
-        self.applicable_if(
-            YES, field='when_pregnant', field_applicable='gestational_weeks')
+        self.required_if(
+            YES,
+            field='when_pregnant',
+            field_required='gestational_weeks')
+
         self.applicable_if(
             YES, field='when_pregnant', field_applicable='pregnancy_hiv_tested')
         self.applicable_if(
